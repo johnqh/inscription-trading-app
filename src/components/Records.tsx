90be3bb09ca05ae9ba79bf5d8d92e2473b87d073
Ritvik Nayak <rnayak@usfca.edu>
2024-04-12 12:47:37 -0700

    Add component for historical records

---
+++ src/components/Records.tsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import { Card, List, ConfigProvider, Space } from "antd";
+import { FrownOutlined } from "@ant-design/icons";
+
+export type Record = {
+    id: number,
+    address: string,
+    action: string,
+    token_size: number,
+    token: string,
+    price?: number,
+    fee?: number,
+    btc_amount?: number,
+    datetime: string
+}
+
+const customizeRenderEmpty = () => (
+  <div style={{ textAlign: "center" }}>
+    <FrownOutlined style={{ fontSize: 20 }} />
+    <p>No records</p>
+  </div>
+);
+
+function Records({ records }: { records: Record[] }) {
+  const headStyle = {
+    backgroundColor: "#5D647B",
+    color: "#f5f5f5",
+  };
+  return (
+   <div>
+    <Space direction="vertical" size="middle" style={{ display: "flex" }}>
+      <Card
+        title="Records"
+        styles={{ header: headStyle }}
+        bordered={true}
+        style={{}}
+      >
+          {records.length}
+      </Card>
+      <ConfigProvider renderEmpty={customizeRenderEmpty}>
+        <List
+          bordered
+          dataSource={records}
+          renderItem={(record) => {
+            let date = new Date(record.datetime);
+            let price: string = (record.price) ? `${record.price}` : "market price";
+            return (<p>{record.action} {record.token_size} {record.token} at {price} <em>(Updated {date.toLocaleString()})</em></p>)
+          }}
+        >
+        </List>
+      </ConfigProvider>
+    </Space>
+   </div>
+  )
+}
+
+export default Records;
